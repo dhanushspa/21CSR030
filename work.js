060da2b9dca44f21a8ac2b5bc693fa68cfa54c77
dhanushspa <117385176+dhanushspa@users.noreply.github.com>
2024-06-18 12:14:13 +0530

    work.js

--- work.js
+++ work.js
@@ -2,7 +2,7 @@ document.getElementById('fetchButton').addEventListener('click', fetchApiRespons
 
 function fetchApiResponse() {
     fetch('http://20.244.56.144/test/rand', {
-        method: 'GET', // or 'POST' if needed
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE4NjkyNTc4LCJpYXQiOjE3MTg2OTIyNzgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6Ijg4OGQxZDMxLTljYTctNGI1My1iMDIyLTgwY2NkZDc4MTAzMyIsInN1YiI6ImRoYW51c2hzcGExQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6InNwTWFydCIsImNsaWVudElEIjoiODg4ZDFkMzEtOWNhNy00YjUzLWIwMjItODBjY2RkNzgxMDMzIiwiY2xpZW50U2VjcmV0IjoiWHB6WEtucXVzd0Rzb2pMUyIsIm93bmVyTmFtZSI6IkRoYW51c2giLCJvd25lckVtYWlsIjoiZGhhbnVzaHNwYTFAZ21haWwuY29tIiwicm9sbE5vIjoiMjFDU1IwMzAifQ.hPNY2QyB80bIN5tzvCpNAmlYo1Eql3Nwn9TaucJ_uBA ' // if needed
